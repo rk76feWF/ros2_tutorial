1f19ecedfa049f4b6ceefdde1fa1e48a350535ab
rk76fewf <80165725+rk76feWF@users.noreply.github.com>
2022-04-28 16:11:41 +0900

    tutorial_topicにコメントを追加

--- tutorial_topic/src/publisher.cpp
+++ tutorial_topic/src/publisher.cpp
@@ -3,21 +3,25 @@
 
 int main(int argc, char **argv)
 {
+    // プロセスの初期化
     rclcpp::init(argc, argv);
+    // ノードの初期化
     auto node = rclcpp::Node::make_shared("publicher");
-
+    // パブリッシャーの初期化
     auto pub = node->create_publisher<std_msgs::msg::Int32>("number", 10);
-
+    // ループ周期の設定
     rclcpp::WallRate loop(1);
+    // パブリッシュするメッセージの宣言
     std_msgs::msg::Int32 msg;
-
+    // CTRL+Cで中断されるまで無限ループ
     while(rclcpp::ok()) {
         msg.data++;
         RCLCPP_INFO(node->get_logger(), "I publish : %d", msg.data);
-
+        // メッセージのパブリッシュ
         pub->publish(msg);
         loop.sleep();
     }
+    // プロセスの終了
     rclcpp::shutdown();
     return 0;
 }
--- tutorial_topic/src/subscriber.cpp
+++ tutorial_topic/src/subscriber.cpp
@@ -1,8 +1,9 @@
 #include <rclcpp/rclcpp.hpp>
 #include <std_msgs/msg/int32.hpp>
-
+// グローバル変数の宣言
 rclcpp::Node::SharedPtr g_node = nullptr;
 
+// Int32型をサブスクライブするコールバック関数
 void onNumberSubscribed(const std_msgs::msg::Int32::SharedPtr msg)
 {
     RCLCPP_INFO(g_node->get_logger(), "I heard : %d", msg->data);
@@ -10,12 +11,15 @@ void onNumberSubscribed(const std_msgs::msg::Int32::SharedPtr msg)
 
 int main(int argc, char **argv)
 {
+    // プロセスの初期化
     rclcpp::init(argc, argv);
+    // ノードの初期化
     g_node = rclcpp::Node::make_shared("subscriber");
-
+    // サブスクライバの初期化
     auto sub = g_node->create_subscription<std_msgs::msg::Int32>("number", 10, onNumberSubscribed);
-
+    // コールバック関数の発生を待機
     rclcpp::spin(g_node);
+    // プロセスの終了
     g_node = nullptr;
     rclcpp::shutdown();
     return 0;
