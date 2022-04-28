84875249d949eed099ccb4086555e5a75283edec
rk76fewf <80165725+rk76feWF@users.noreply.github.com>
2022-04-28 15:13:38 +0900

    tutorial_packageにコメントを追加

--- tutorial_package/src/helloworld.cpp
+++ tutorial_package/src/helloworld.cpp
@@ -2,14 +2,18 @@
 
 int main(int argc, char ** argv)
 {
+    // プロセスの初期化
     rclcpp::init(argc, argv);
+    // ノードの初期化
     auto node = rclcpp::Node::make_shared("helloworld");
+    // ループ間隔を1Hzに設定
     rclcpp::WallRate loop(1);
-
+    // CTRL+Cで中断されるまで無限ループ
     while(rclcpp::ok()) {
         RCLCPP_INFO(node->get_logger(), "Hello World!");
         loop.sleep();
     }
+    // プロセスの終了
     rclcpp::shutdown();
     return 0;
 }
